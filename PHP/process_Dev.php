1fd2af0e38bac4956d57da39bb10b25207f1e389
alissonthx <50666664+alissonthx@users.noreply.github.com>
2021-11-29 12:38:15 -0300

    Correção devolução de emprestimos

--- PHP/process_Dev.php
+++ PHP/process_Dev.php
@@ -1,8 +1,11 @@
 <?php
+session_start();
 include_once "conection.php";
 
 $id_session = $_SESSION["id"];
-$devolution = "DELETE FROM emprestimos WHERE $id_session";
+$id_book = $_GET["id"];
+
+$devolution = "DELETE FROM emprestimos WHERE emprestimos.id_user = $id_session AND id_book = $id_book";
 $result_emp = mysqli_query($ccon, $devolution);
 print_r(mysqli_error($ccon));
 header("Location:dashboard_Emp.php");
